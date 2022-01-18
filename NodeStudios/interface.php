1f1b9c01eb3be49074d98f92f4f4c90520485ef6
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-01-18 18:35:25 -0300

    Arrumando

---
+++ NodeStudios/interface.php
@@ -0,0 +1,29 @@
+<?php
+
+interface Crud{
+    public function create();
+    public function read();
+    public function update();
+    public function delete();
+    
+}
+
+class Noticias implements Crud{
+    
+    public function create(){
+        //logica para criar uma noticia
+    }
+
+
+    public function read(){
+        //logica para ler uma noticia
+    }
+
+    public function update(){
+        //logica para atualizar uma noticia
+    }
+
+    public function delete(){
+        //logica para deletar uma noticia
+    }
+}
