832b27fd316131cbb6b5c3632618cecef8aec29b
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-02-24 22:56:58 -0300

    Add

--- CursoEmVideoPOO/controleRemoto.php
+++ CursoEmVideoPOO/controleRemoto.php
@@ -6,7 +6,20 @@ class Controle{
     public $pilha;
     
     public function ligarControle(){
-        $this->ligar == "Controle Ligado";
+        $this->ligar = true;
+        echo "Controle Ligado";
+    }
+    public function desligarControle(){
+        $this->ligar = false;
+        echo "Controle desligado";
     }
 }
+
+$controle1 = new Controle();
+$controle1->ligar = false;
+$controle1->botoes = "2";
+$controle1->pilha = "Palito";
+
+echo $controle1->ligarControle() . PHP_EOL;
+echo $controle1->desligarControle();
 
