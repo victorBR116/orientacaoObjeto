aa913beffe7db85dfae260b55a163d8da396ff88
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-03-23 19:23:02 -0300

    boraaaa

--- CursoEmVideoPOO/EncapsControle.php
+++ CursoEmVideoPOO/EncapsControle.php
@@ -1,3 +1,6 @@
 <?php
 
 //aprendendo sobre encapsulamento.
+///criaremos um controlador que vai realizar as funções a baixo
+//controlador - ligar, Desligar, abrir menu, fechar menu, mais volume, menos volume, ligar mudo, desligar mudo, play, pause.
+///atribustos privados para encapsulamento
