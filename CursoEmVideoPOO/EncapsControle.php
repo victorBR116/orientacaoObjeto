ee8c4c2ceb8c8480b5d70b90bf925c58929253d4
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-03-23 19:38:14 -0300

    já volto

--- CursoEmVideoPOO/EncapsControle.php
+++ CursoEmVideoPOO/EncapsControle.php
@@ -1,5 +1,18 @@
 <?php
+//já volto
+interface controlador {
+    public abstract function ligar();
+    public abstract function desligar();
+    public abstract function abrirMenu();
+    public abstract function fecharMenu();
+    public abstract function maisVolume();
+    public abstract function menosVolume();
+    public abstract function ligarMudo();
+    public abstract function desligarMudo();
+    public abstract function play();
+    public abstract function pause();
 
+}
 //aprendendo sobre encapsulamento.
 ///criaremos um controlador que vai realizar as funções a baixo
 //controlador - ligar, Desligar, abrir menu, fechar menu, mais volume, menos volume, ligar mudo, desligar mudo, play, pause.
