0f0748296d8900b033422947f9bd9f3be828d24b
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-03-24 23:17:49 -0300

    Vorto já

--- CursoEmVideoPOO/ControleaAula06.php
+++ CursoEmVideoPOO/ControleaAula06.php
@@ -1,5 +1,40 @@
 <?php
 
-class controle{
+class ControleRemoto{
+    private $volume;
+    private $ligado;
+    private $tocando;
     
+    //metodos especiais
+
+    function __construct()
+    {
+        $this->volume = 50;
+        $this-> ligado = false;
+        $this->tocando = false;
+    }
+
+    function getVolume(){
+        return $this->volume;
+    }
+
+    function setVolume($v){
+        $this->volume = $v;
+    }
+
+    function getLigado(){
+        return $this->ligado;
+    }
+
+    function setLigado($l){
+        $this->ligado = $l;
+    }
+
+    function getTocando(){
+        return $this->tocando;
+    }
+
+    function setTocando($t){
+        $this->tocando = $t;
+    }
 }
