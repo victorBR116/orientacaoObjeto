a2642e012d05dab0ba75a0d0f6667d76e2b9e4be
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-05-12 00:10:58 -0300

    att

---
+++ CursoEmVideoPOO/indexaula6.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 06 encaps</title>
+</head>
+<body>
+    <?
+    require_once "E:\Estudos\Alura\Orientação Objeto\CursoEmVideoPOO\EncapsControle.php";
+    
+    ?>
+</body>
+</html>
