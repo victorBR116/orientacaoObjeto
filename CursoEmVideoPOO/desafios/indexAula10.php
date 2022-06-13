ac6397164d1858b1f8cefe642cfa54c802037a45
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-06-13 17:14:55 -0300

    att

--- CursoEmVideoPOO/desafios/indexAula10.php
+++ CursoEmVideoPOO/desafios/indexAula10.php
@@ -17,7 +17,7 @@
 
         $j[0] = new Aluno(true, "123131", "Victor", 21);
         $j[0] = new Pessoa("Victor Hugo", 21, "M");
-        $j[1] = new Funcionario('Ingles', true);
+        $j[1] = new Funcionario('Limpeza', true);
         $j[2] = new pessoa('victor', 14, 'M');
         $j[3] = new Professor('Ingles', 2100 );
        /* $j[0]->setNome("Victor Hugo Santos");
@@ -28,10 +28,11 @@
         $j[3]->setNome("carlos");
         $j[3]->setIdade(43);
         $j[3]->setSexo("M");
-        //$j[3]->receberAumento();
+        //$j[1]->mudarTrabalho();
+        //$j[1]->mudarTrabalho()
         //$j[3]->setsalario(123);
         // $j[3]->getsalario();    
-        var_dump($j[3]);
+        var_dump($j[1]);
     ?>
     </pre>
 </body>
